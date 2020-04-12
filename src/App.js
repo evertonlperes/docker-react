40ce0e11477b8df14269f390599b8273bbdbb9be
Everton Peres <evertonluperes@gmail.com>
2020-04-12 15:42:48 -0400

    AWS Deploy setup

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Let's see....
+          Junior e Michelle - Quero a sobremesa de sonho de valsa!!!!
         </p>
         <a
           className="App-link"
